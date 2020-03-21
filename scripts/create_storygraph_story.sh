9c6d3f1b07c35275a9dd1ac459d827cae40f0828
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-03-20 18:54:42 -0600

    updates to script, main page, and template

--- scripts/create_storygraph_story.sh
+++ scripts/create_storygraph_story.sh
@@ -26,46 +26,41 @@ echo "`date` --- using year: ${sg_year} ; month: ${sg_month}; date: ${sg_date}"
 
 hr_sg_date="${sg_year}-${sg_month}-${sg_date}"
 echo hr_sg_date=$hr_sg_date
-# generate mementos from storygraph with sgtk
 
-if [ ! -e ${working_directory}/story-mementos.tsv ]; then
+# 1. query StoryGraph service for rank r story of the day
+if [ ! -e ${working_directory}/story-original-resources.tsv ]; then
 
     sg_file="${working_directory}/sgtk-maxgraph-${sg_year}${sg_month}${sg_date}.json"
 
     echo "creating StoryGraph file ${sg_file}"
 
-    sgtk --pretty-print -o ${sg_file} maxgraph \
-        --start-mm-dd ${sg_month}-${sg_date} --end-mm-dd ${sg_month}-${sg_date} \
-        --daily-maxgraph-count 1 --year ${sg_year}
-    sg_url=`grep '"graph_uri":' ${sg_file} | sed 's/^[ ]*"graph_uri": "//g' | sed 's/"[,]*$//g'`
+    # sgtk --pretty-print -o ${sg_file} maxgraph \
+    #     --start-mm-dd ${sg_month}-${sg_date} --end-mm-dd ${sg_month}-${sg_date} \
+    #     --daily-maxgraph-count 1 --year ${sg_year}
+    sgtk --pretty-print -o ${working_directory}/graphs_links.txt maxgraph \
+        --daily-maxgraph-count=0 -y ${sg_year} --start-mm-dd=${sg_month}-${sg_date} --end-mm-dd=${sg_month}-${sg_date} \
+        --cluster-stories --format=maxstory_links --maxstory-count=1 > ${working_directory}/sg-output.txt 2>&1
+    sg_base_uri=`cat ${working_directory}/sg-output.txt | grep "service uri:" | awk '{ print $3 }'`
+    sg_fragment=`cat ${working_directory}/sg-output.txt | grep "maxgraph cursor:" | awk '{ print $3 }'`
+    echo "${sg_base_uri}${sg_fragment}" > ${working_directory}/sg.url.txt
+    # grep '"graph_uri":' ${sg_file} | sed 's/^[ ]*"graph_uri": "//g' | sed 's/"[,]*$//g' > ${working_directory}/sg.url.txt
 
     echo "URI-Rs" > ${working_directory}/story-original-resources.tsv
-    grep '"link":' ${sg_file} | sed 's/^[ ]*"link": "//g' | sed 's/"$//g' >> ${working_directory}/story-original-resources.tsv
+    cat ${working_directory}/graphs_links.txt >> ${working_directory}/story-original-resources.tsv
+    # grep '"link":' ${sg_file} | sed 's/^[ ]*"link": "//g' | sed 's/"$//g' >> ${working_directory}/story-original-resources.tsv
 
+else
+    echo "already discovered ${working_directory}/story-original-resources.tsv so moving on to next command..."
+fi
+
+# 2. Create URI-Ms from URI-Rs
+if [ ! -e ${working_directory}/story-mementos.tsv ]; then
     hc identify mementos -i original-resources -a ${working_directory}/story-original-resources.tsv -cs mongodb://localhost/csStoryGraph -o ${working_directory}/story-mementos.tsv
-
 else
     echo "already discovered ${working_directory}/story-mementos.tsv so moving on to next command..."
 fi
 
-# perform image analysis for story image
-if [ ! -e ${working_directory}/imagedata.json ]; then
-    image_analysis_cmd="hc report image-data -i mementos -a ${working_directory}/story-mementos.tsv -cs mongodb://localhost/csStoryGraph -o ${working_directory}/imagedata.json"
-    echo "`date` --- executing command::: ${image_analysis_cmd}"
-    ${image_analysis_cmd}
-else
-    echo "already discovered ${working_directory}/imagedata.json so moving on to next command..."
-fi
-
-# perform sumgram analysis
-if [ ! -e ${working_directory}/sumgram_data.tsv ]; then
-    echo "`date` --- executing command"
-    hc report terms -i mementos -a ${working_directory}/story-mementos.tsv -cs mongodb://localhost/csStoryGraph -o ${working_directory}/sumgram_data.tsv --sumgrams
-else
-    echo "already discovered ${working_directory}/sumgram_data.tsv so moving on to next command..."
-fi
-
-# perform entity analysis
+# 3. Generate entity report
 if [ ! -e ${working_directory}/entity_data.tsv ]; then
     echo "`date` --- executing command"
     hc report entities -i mementos -a ${working_directory}/story-mementos.tsv -cs mongodb://localhost/csStoryGraph -o ${working_directory}/entity_data.tsv
@@ -73,7 +68,23 @@ else
     echo "already discovered ${working_directory}/entity_data.tsv so moving on to next command..."
 fi
 
-# sort by publication date
+# 4. Generate sumgram report
+if [ ! -e ${working_directory}/sumgram_data.tsv ]; then
+    echo "`date` --- executing command"
+    hc report terms -i mementos -a ${working_directory}/story-mementos.tsv -cs mongodb://localhost/csStoryGraph -o ${working_directory}/sumgram_data.tsv --sumgrams
+else
+    echo "already discovered ${working_directory}/sumgram_data.tsv so moving on to next command..."
+fi
+
+# 5. Generate image report
+if [ ! -e ${working_directory}/imagedata.json ]; then
+    echo "`date` --- executing command"
+    hc report image-data -i mementos -a ${working_directory}/story-mementos.tsv -cs mongodb://localhost/csStoryGraph -o ${working_directory}/imagedata.json
+else
+    echo "already discovered ${working_directory}/imagedata.json so moving on to next command..."
+fi
+
+# 6. Order URI-Ms by publication date
 if [ ! -e ${working_directory}/sorted-story-mementos.tsv ]; then
     echo "`date` --- executing command:::"
     hc order pubdate-else-memento-datetime -i mementos -a ${working_directory}/story-mementos.tsv -o ${working_directory}/sorted-story-mementos.tsv -cs mongodb://localhost/csStoryGraph
@@ -81,7 +92,7 @@ else
     echo "already discovered ${working_directory}/sorted-story-mementos.tsv so moving on to next command..."
 fi
 
-# generate story JSON for raintale with hc
+# 7. Consolidate reports and URI-M list to generate Raintale story data
 if [ ! -e ${working_directory}/raintale-story.json ]; then
     echo "`date` --- executing command:::"
     hc synthesize raintale-story -i mementos -a ${working_directory}/story-mementos.tsv -o ${working_directory}/raintale-story.json -cs mongodb://localhost/csStoryGraph --imagedata ${working_directory}/imagedata.json --title "StoryGraph Biggest Story ${hr_sg_date}" --termdata ${working_directory}/sumgram_data.tsv --entitydata ${working_directory}/entity_data.tsv
@@ -90,18 +101,17 @@ else
 fi
 
 post_date=`date '+%Y-%m-%d'`
-# tellstory using story JSON, save to _posts
+# 8. Generate Jekyll HTML file for the day's rank r story
 if [ ! -e _posts/${post_date}-storygraph-bigstory.html ]; then
     echo "`date` --- executing command:::"
-    tellstory -i ${working_directory}/raintale-story.json --storyteller template --story-template raintale-templates/storygraph-story.html -o _posts/${post_date}-storygraph-bigstory.html
+    sg_url=`cat ${working_directory}/sg.url.txt`
+    tellstory -i ${working_directory}/raintale-story.json --storyteller template --story-template raintale-templates/storygraph-story.html -o _posts/${post_date}-storygraph-bigstory.html --collection-url ${sg_url}
 else
     echo "already created story at _posts/${post_date}-storygraph-bigstory.html"
 fi
 
-# commit
+# 9. Publish to GitHub Pages
 git pull
 git add _posts/${post_date}-storygraph-bigstory.html
 git commit -m "adding storygraph story for ${post_date}"
-
-# push
 git push

d609e82947439ea8f5e0771ba6f3fa4010d30537
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-03-24 01:49:33 -0600

    more cleanup and fixes

--- scripts/create_storygraph_story.sh
+++ scripts/create_storygraph_story.sh
@@ -25,8 +25,18 @@ fi
 echo "`date` --- using working directory ${working_directory}"
 echo "`date` --- using year: ${sg_year} ; month: ${sg_month}; date: ${sg_date}"
 
+original_resource_file=${working_directory}/story-original-resources.tsv
+mementos_file=${working_directory}/story-mementos.tsv
+entity_report=${working_directory}/entity_data.tsv
+sumgram_report=${working_directory}/sumgram_data.tsv
+image_report=${working_directory}/imagedata.json
+sorted_mementos_file=${working_directory}/sorted-story-mementos.tsv
+story_data_file=${working_directory}/raintale-story.json
+jekyll_story_file=_posts/${post_date}-storygraph-bigstory.html
+small_striking_image=assets/img/storygraph_striking_images/${post_date}.png
+
 # 1. query StoryGraph service for rank r story of the day
-if [ ! -e ${working_directory}/story-original-resources.tsv ]; then
+if [ ! -e ${original_resource_file} ]; then
 
     sg_file="${working_directory}/sgtk-maxgraph-${sg_year}${sg_month}${sg_date}.json"
 
@@ -39,82 +49,82 @@ if [ ! -e ${working_directory}/story-original-resources.tsv ]; then
     sg_fragment=`cat ${working_directory}/sg-output.txt | grep "maxgraph cursor:" | awk '{ print $3 }'`
     echo "${sg_base_uri}${sg_fragment}" > ${working_directory}/sg.url.txt
 
-    echo "URI-Rs" > ${working_directory}/story-original-resources.tsv
-    cat ${working_directory}/graphs_links.txt >> ${working_directory}/story-original-resources.tsv
-
+    echo "URI-Rs" > ${original_resource_file}
+    cat ${working_directory}/graphs_links.txt >> ${original_resource_file}
 else
-    echo "already discovered ${working_directory}/story-original-resources.tsv so moving on to next command..."
+    echo "already discovered ${original_resource_file} so moving on to next command..."
 fi
 
 # 2. Create URI-Ms from URI-Rs
-if [ ! -e ${working_directory}/story-mementos.tsv ]; then
-    hc identify mementos -i original-resources -a ${working_directory}/story-original-resources.tsv -cs mongodb://localhost/csStoryGraph -o ${working_directory}/story-mementos.tsv
+if [ ! -e ${mementos_file} ]; then
+    hc identify mementos -i original-resources -a ${original_resource_file} -cs mongodb://localhost/csStoryGraph -o ${mementos_file}
 else
-    echo "already discovered ${working_directory}/story-mementos.tsv so moving on to next command..."
+    echo "already discovered ${mementos_file} so moving on to next command..."
 fi
 
 # 3. Generate entity report
-if [ ! -e ${working_directory}/entity_data.tsv ]; then
+if [ ! -e ${entity_report} ]; then
     echo "`date` --- executing command"
-    hc report entities -i mementos -a ${working_directory}/story-mementos.tsv -cs mongodb://localhost/csStoryGraph -o ${working_directory}/entity_data.tsv
+    hc report entities -i mementos -a ${mementos_file} -cs mongodb://localhost/csStoryGraph -o ${entity_report}
 else
-    echo "already discovered ${working_directory}/entity_data.tsv so moving on to next command..."
+    echo "already discovered ${entity_report} so moving on to next command..."
 fi
 
 # 4. Generate sumgram report
-if [ ! -e ${working_directory}/sumgram_data.tsv ]; then
+if [ ! -e ${sumgram_report} ]; then
     echo "`date` --- executing command"
-    hc report terms -i mementos -a ${working_directory}/story-mementos.tsv -cs mongodb://localhost/csStoryGraph -o ${working_directory}/sumgram_data.tsv --sumgrams
+    hc report terms -i mementos -a ${mementos_file} -cs mongodb://localhost/csStoryGraph -o ${sumgram_report} --sumgrams
 else
-    echo "already discovered ${working_directory}/sumgram_data.tsv so moving on to next command..."
+    echo "already discovered ${sumgram_report} so moving on to next command..."
 fi
 
 # 5. Generate image report
-if [ ! -e ${working_directory}/imagedata.json ]; then
+if [ ! -e ${image_report} ]; then
     echo "`date` --- executing command"
-    hc report image-data -i mementos -a ${working_directory}/story-mementos.tsv -cs mongodb://localhost/csStoryGraph -o ${working_directory}/imagedata.json
+    hc report image-data -i mementos -a ${mementos_file} -cs mongodb://localhost/csStoryGraph -o ${image_report}
 else
-    echo "already discovered ${working_directory}/imagedata.json so moving on to next command..."
+    echo "already discovered ${image_report} so moving on to next command..."
 fi
 
 # 6. Order URI-Ms by publication date
-if [ ! -e ${working_directory}/sorted-story-mementos.tsv ]; then
+if [ ! -e ${sorted_mementos_file} ]; then
     echo "`date` --- executing command:::"
-    hc order pubdate-else-memento-datetime -i mementos -a ${working_directory}/story-mementos.tsv -o ${working_directory}/sorted-story-mementos.tsv -cs mongodb://localhost/csStoryGraph
+    hc order pubdate-else-memento-datetime -i mementos -a ${mementos_file} -o ${sorted_mementos_file} -cs mongodb://localhost/csStoryGraph
 else
     echo "already discovered ${working_directory}/sorted-story-mementos.tsv so moving on to next command..."
 fi
 
 # 7. Consolidate reports and URI-M list to generate Raintale story data
-if [ ! -e ${working_directory}/raintale-story.json ]; then
+if [ ! -e ${story_data_file} ]; then
     echo "`date` --- executing command:::"
-    hc synthesize raintale-story -i mementos -a ${working_directory}/story-mementos.tsv -o ${working_directory}/raintale-story.json -cs mongodb://localhost/csStoryGraph --imagedata ${working_directory}/imagedata.json --title "StoryGraph Biggest Story ${post_date}" --termdata ${working_directory}/sumgram_data.tsv --entitydata ${working_directory}/entity_data.tsv
+    hc synthesize raintale-story -i mementos -a ${mementos_file} -o ${story_data_file} -cs mongodb://localhost/csStoryGraph --imagedata ${image_report} --title "StoryGraph Biggest Story ${post_date}" --termdata ${sumgram_report} --entitydata ${entity_report}
 else
-    echo "already discovered ${working_directory}/raintale-story.json so moving on to next command..."
+    echo "already discovered ${story_data_file} so moving on to next command..."
 fi
 
 # 8. Generate Jekyll HTML file for the day's rank r story
-if [ ! -e _posts/${post_date}-storygraph-bigstory.html ]; then
+if [ ! -e ${jekyll_story_file} ]; then
     echo "`date` --- executing command:::"
     sg_url=`cat ${working_directory}/sg.url.txt`
-    tellstory -i ${working_directory}/raintale-story.json --storyteller template --story-template raintale-templates/storygraph-story.html -o _posts/${post_date}-storygraph-bigstory.html --collection-url ${sg_url}
+    tellstory -i ${story_data_file} --storyteller template --story-template raintale-templates/storygraph-story.html -o ${jekyll_story_file} --collection-url ${sg_url}
 else
-    echo "already created story at _posts/${post_date}-storygraph-bigstory.html"
+    echo "already created story at ${jekyll_story_file}"
 fi
 
-if [ ! -e assets/img/storygraph_striking_images/${post_date}.png ]; then
-    striking_image_url=`grep "^img:" _posts/${post_date}-storygraph-bigstory.html | awk '{ print $2 }'`
+# extra - swap the striking image with a smaller thumbnail so that the main page will load faster
+if [ ! -e ${small_striking_image} ]; then
+    striking_image_url=`grep "^img:" ${jekyll_story_file} | awk '{ print $2 }'`
     wget -O ${working_directory}/${post_date}-striking-image.dat ${striking_image_url}
     convert ${working_directory}/${post_date}-striking-image.dat ${working_directory}/${post_date}-striking-image-origsize.png
-    convert ${working_directory}/${post_date}-striking-image-origsize.png -resize 368.391x245.531 assets/img/storygraph_striking_images/${post_date}.png
-    sed -i '' -e "s|^img: .*$|img: /dsa-puddles/assets/img/striking_images/${post_date}.png|g" _posts/${post_date}-storygraph-bigstory.html
+    convert ${working_directory}/${post_date}-striking-image-origsize.png -resize 368.391x245.531 ${small_striking_image}
+    sed -i '' -e "s|^img: .*$|img: /dsa-puddles/${small_striking_image}|g" ${jekyll_story_file}
 else
-    echo "already generated smaller striking image for assets/img/striking_images/${post_date}.png"
+    echo "already generated smaller striking image for ${small_striking_image}"
 fi
 
 # 9. Publish to GitHub Pages
 git pull
-git add _posts/${post_date}-storygraph-bigstory.html
-git add assets/img/storygraph_striking_images/${post_date}.png
+git add ${jekyll_story_file}
+git add ${small_striking_image}
 git commit -m "adding storygraph story for ${post_date}"
 git push

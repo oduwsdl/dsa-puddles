77080cc9a1fe1735b5f226e303edfc3d32defc03
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-03-24 01:17:22 -0600

    more cleanup from bad run

--- scripts/create_multiple_storygraph_stories.sh
+++ scripts/create_multiple_storygraph_stories.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 for story_date in "$@"; do
-    scripts/create_storygraph_story.sh ${story_date}
+    echo "Generating story for ${story_date}"
+
+    scripts/create_storygraph_story.sh ${story_date} ~/tmp/working-multiple-stories
     status=$?
 
     if [ ${status} -ne 0 ]; then
--- scripts/create_storygraph_story.sh
+++ scripts/create_storygraph_story.sh
@@ -13,16 +13,17 @@ else
     sg_year=`echo $1 | awk -F- '{ print $1 }'`
 fi
 
+post_date="${sg_year}-${sg_month}-${sg_date}"
+
 if [ -z $2 ]; then
     working_directory=`mktemp -d -t storygraph-stories-`
 else
-    working_directory=$2
+    working_directory=$2/${post_date}
     mkdir -p ${working_directory}
 fi
 
 echo "`date` --- using working directory ${working_directory}"
 echo "`date` --- using year: ${sg_year} ; month: ${sg_month}; date: ${sg_date}"
-post_date="${sg_year}-${sg_month}-${sg_date}"
 
 # 1. query StoryGraph service for rank r story of the day
 if [ ! -e ${working_directory}/story-original-resources.tsv ]; then
@@ -87,14 +88,13 @@ fi
 # 7. Consolidate reports and URI-M list to generate Raintale story data
 if [ ! -e ${working_directory}/raintale-story.json ]; then
     echo "`date` --- executing command:::"
-    hc synthesize raintale-story -i mementos -a ${working_directory}/story-mementos.tsv -o ${working_directory}/raintale-story.json -cs mongodb://localhost/csStoryGraph --imagedata ${working_directory}/imagedata.json --title "StoryGraph Biggest Story ${hr_sg_date}" --termdata ${working_directory}/sumgram_data.tsv --entitydata ${working_directory}/entity_data.tsv
+    hc synthesize raintale-story -i mementos -a ${working_directory}/story-mementos.tsv -o ${working_directory}/raintale-story.json -cs mongodb://localhost/csStoryGraph --imagedata ${working_directory}/imagedata.json --title "StoryGraph Biggest Story ${post_date}" --termdata ${working_directory}/sumgram_data.tsv --entitydata ${working_directory}/entity_data.tsv
 else
     echo "already discovered ${working_directory}/raintale-story.json so moving on to next command..."
 fi
 
 # 8. Generate Jekyll HTML file for the day's rank r story
 if [ ! -e _posts/${post_date}-storygraph-bigstory.html ]; then
-    hr_sg_date="${sg_year}-${sg_month}-${sg_date}"
     echo "`date` --- executing command:::"
     sg_url=`cat ${working_directory}/sg.url.txt`
     tellstory -i ${working_directory}/raintale-story.json --storyteller template --story-template raintale-templates/storygraph-story.html -o _posts/${post_date}-storygraph-bigstory.html --collection-url ${sg_url}
@@ -102,11 +102,11 @@ else
     echo "already created story at _posts/${post_date}-storygraph-bigstory.html"
 fi
 
-if [ ! -e assets/img/striking_images/${post_date}.png ]; then
+if [ ! -e assets/img/storygraph_striking_images/${post_date}.png ]; then
     striking_image_url=`grep "^img:" _posts/${post_date}-storygraph-bigstory.html | awk '{ print $2 }'`
     wget -O ${working_directory}/${post_date}-striking-image.dat ${striking_image_url}
     convert ${working_directory}/${post_date}-striking-image.dat ${working_directory}/${post_date}-striking-image-origsize.png
-    convert ${working_directory}/${post_date}-striking-image-origsize.png -resize 368.391x245.531 assets/img/striking_images/${post_date}.png
+    convert ${working_directory}/${post_date}-striking-image-origsize.png -resize 368.391x245.531 assets/img/storygraph_striking_images/${post_date}.png
     sed -i '' -e "s|^img: .*$|img: /dsa-puddles/assets/img/striking_images/${post_date}.png|g" _posts/${post_date}-storygraph-bigstory.html
 else
     echo "already generated smaller striking image for assets/img/striking_images/${post_date}.png"
@@ -115,5 +115,6 @@ fi
 # 9. Publish to GitHub Pages
 git pull
 git add _posts/${post_date}-storygraph-bigstory.html
+git add assets/img/storygraph_striking_images/${post_date}.png
 git commit -m "adding storygraph story for ${post_date}"
 git push

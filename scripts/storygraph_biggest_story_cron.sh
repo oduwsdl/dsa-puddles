94f23807ef81715ce1b787fe1d49ec6045a0a585
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-03-29 11:10:48 -0600

    script updates

--- scripts/storygraph_biggest_story_cron.sh
+++ scripts/storygraph_biggest_story_cron.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+source /usr/local/bin/virtualenvwrapper.sh
+
+post_date=$1
+working_directory=$2
+
 workon storygraph-stories
-/Users/smj/Unsynced-Projects/dsa-puddles/scripts/create_storygraph_story.sh > /Users/smj/Unsynced-Projects/dsa-puddles-logs/storygraph-biggest-`date '+%Y%m%d%H%M%S'`.log 2>&1
+/Users/smj/Unsynced-Projects/dsa-puddles/scripts/create_storygraph_story.sh ${post_date} ${working_directory} > /Users/smj/Unsynced-Projects/dsa-puddles-logs/storygraph-biggest-`date '+%Y%m%d%H%M%S'`.log 2>&1

63e47eb5c52bafcc211d98978ee6db9c20f387ba
Shawn M. Jones <jones.shawn.m@gmail.com>
2020-04-03 17:37:09 -0600

    changes to work around cron issues

--- scripts/tweet_storygraph_todays_biggest_story copy.sh
+++ scripts/tweet_storygraph_todays_biggest_story copy.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-post_date=$1
+echo "starting script"
 
-# post_url=$1
-# post_message=$2
-
-human_readable_date=`/usr/local/bin/gdate --date "${post_date}" '+%A, %B %e, %Y'`
-directory_date=`/usr/local/bin/gdate --date "${post_date}" '+%Y/%m/%d'`
+human_readable_date=`/usr/local/bin/gdate '+%A, %B %e, %Y'`
+directory_date=`/usr/local/bin/gdate '+%Y/%m/%d'`
 
 post_url="https://oduwsdl.github.io/dsa-puddles/stories/shari/${directory_date}/storygraph_biggest_story_${post_date}/"
-post_message="SHARI: StoryGraph's Biggest Story of the day for ${human_readable_date} is now available ${post_url}"
+post_message="StoryGraph's Biggest News Story so far for Today, ${human_readable_date}, is now available. Check back at this URL, because it will be updated as the day goes on. ${post_url}"
 
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 source /usr/local/bin/virtualenvwrapper.sh
---
+++ scripts/tweet_storygraph_yesterdays_biggest_story.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# echo "starting script"
+
+DATE_YESTERDAY=`/usr/local/bin/gdate --date "yesterday" '+%Y-%m-%d'`
+
+post_date=${DATE_YESTERDAY}
+
+# echo "using post date ${post_date}"
+
+human_readable_date=`/usr/local/bin/gdate --date "${post_date}" '+%A, %B %e, %Y'`
+directory_date=`/usr/local/bin/gdate --date "${post_date}" '+%Y/%m/%d'`
+
+post_url="https://oduwsdl.github.io/dsa-puddles/stories/shari/${directory_date}/storygraph_biggest_story_${post_date}/"
+post_message="StoryGraph's Biggest News Story for Yesterday, ${human_readable_date} is now available ${post_url}"
+
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+source /usr/local/bin/virtualenvwrapper.sh
+workon storygraph-stories
+cd /Users/smj/Unsynced-Projects/dsa-puddles
+python ./scripts/tweet_if_post_present.py ~/Unsynced-Projects/dsa-credentials/twitter-credentials.yaml "${post_url}" "${post_message}" > /Users/smj/Unsynced-Projects/dsa-puddles-logs/tweeting-storygraph-biggest-`date '+%Y%m%d%H%M%S'`.log 2>&1
+
+# echo "done running script"
